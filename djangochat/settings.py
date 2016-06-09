b980d05de11567c831b25f0561613d450c60dc31
Alexander Martin <alexander.martin@excella.com>
2016-06-09 12:27:26 -0400

    Added creation of dictionary before using it.

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'djangochat.wsgi.application'
 
 import dj_database_url
 
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
